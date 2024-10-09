d11805e8911b264fea5721d4ab83899322711ae2
Kenski Lao <kennylao@Kennys-MacBook-Pro.local>
2024-10-09 11:59:56 -0700

    updates

---
+++ main.py
@@ -0,0 +1,62 @@
+import spandrel_extra_arches
+from spandrel import ImageModelDescriptor, ModelLoader
+import torch
+from pathlib import Path
+import argparse
+from PIL import Image
+import numpy as np
+
+
+def pil_to_tensor(img: Image.Image) -> torch.Tensor:
+    img = np.array(img.convert("RGB"))
+    img = img[:, :, ::-1]
+    img = np.transpose(img, (2, 0, 1))
+    img = np.ascontiguousarray(img) / 255
+
+    return torch.from_numpy(img).unsqueeze(0).float().cuda()
+
+
+def tensor_to_pil(tensor: torch.Tensor) -> Image.Image:
+    if tensor.ndim == 4:
+        if tensor.shape[0] != 1:
+            raise ValueError(f"{tensor.shape} does not describe a BCHW tensor.")
+
+        tensor.squeeze(0)
+
+    assert tensor.ndim == 3, f"{tensor.shape} does not describe a CHW tensor"
+
+    arr = tensor.float().cpu().clamp_(0, 1).numpy()
+    arr = 255.0 * np.moveaxis(arr, 0, 2)
+    arr = arr.round().astype(np.uint8)
+    arr = arr[:, :, ::-1]
+
+    return Image.fromarray(arr, "RGB")
+
+
+def process(image: torch.Tensor, span_model: any) -> torch.Tensor:
+    with torch.no_grad():
+        upscaled_img = span_model(image)
+
+        return upscaled_img
+
+
+if __name__ == "__main__":
+    print("Starting upscale.")
+
+    spandrel_extra_arches.install()
+    model_path = Path.cwd().joinpath("srformer_4x.pth")
+
+    model = ModelLoader().load_from_file(model_path)
+
+    assert isinstance(model, ImageModelDescriptor)
+
+    model.scale = 4
+    model.cuda().eval()
+
+    path_to_img = Path.cwd().joinpath("input_images", "original.png")
+
+    image = pil_to_tensor(img=Image.open(path_to_img))
+    image = process(image=image, span_model=model)
+    image = tensor_to_pil(tensor=image)
+
+    image.save(Path.cwd().joinpath("output_images", "upscaled.png"))
